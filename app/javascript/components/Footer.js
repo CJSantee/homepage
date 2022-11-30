d548f500b197a6cab3a1fa29bd91b2fc628f1655
CJSantee <santeedev@gmail.com>
2022-11-30 11:40:24 -0600

    Added content from cjsantee.github.io

---
+++ app/javascript/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+// Bootstrap Components
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+// Assets
+import GitHub from "../../assets/images/GitHub.png";
+
+function Footer() {
+  return (
+    // .fixed-bottom for Sticky Footer
+    <Container>
+      <Navbar
+        variant='dark'
+        className='justify-content-center align-items-center'
+      >
+        <Nav>
+          <Nav.Link href='https://github.com/CJSantee/cjsantee.github.io'>
+            {"Developed by "}
+            <img src={GitHub} alt='' width={16} />
+            {" Colin Santee"}
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    </Container>
+  );
+}
+
+export default Footer;
