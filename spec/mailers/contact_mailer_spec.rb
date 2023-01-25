8f0f7f9f52ef9ef9edcc50acf9021c0b5285ccd6
CJSantee <santeedev@gmail.com>
2023-01-25 11:09:22 -0600

    Rspec Tests

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe ContactMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'sends a email with contact form info' do 
+    contact_form = ContactForm.new(full_name: "Joe Smith", email: "joe@gmail.com", message: "I want to work together!")
+
+		email = ContactMailer.with(contact_form: contact_form).contact_email
+
+		# Check if the email is sent
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+		# Check the contents are correct
+    assert_equal [ENV['GMAIL_ACCOUNT']], email.from
+    assert_equal [ENV['ADMIN_EMAIL']], email.to
+    assert_equal "New message from #{contact_form.full_name}!", email.subject
+    assert_match contact_form.full_name, email.html_part.body.encoded
+    assert_match contact_form.full_name, email.text_part.body.encoded
+    assert_match contact_form.email, email.html_part.body.encoded
+    assert_match contact_form.email, email.text_part.body.encoded
+    assert_match contact_form.message, email.html_part.body.encoded
+    assert_match contact_form.message, email.text_part.body.encoded
+	end
 end
--- spec/requests/contact_forms_spec.rb
+++ spec/requests/contact_forms_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "ContactForms", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /contact" do
+    it 'creates a new contact form' do
+			post '/contact', :params => {
+				contact_form: {
+					full_name: 'John Smith',
+					email: 'johnsmith@gmail.com',
+					message: 'Hi! I\'d like to work together!',
+				},
+			}
+			expect(response).to have_http_status(:created)
+		end
   end
 end
