789d86628fff05d544795e677e4220cce44436f6
CJSantee <santeedev@gmail.com>
2023-07-08 08:13:58 -0500

    A Fresh Start.

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-	root 'pages#home'
-
-	get '/resume' => 'pages#download_resume'
-end
